833198fb12066dabb4bc698ffc8212b2636f3a90
JanDam04 <118687318+JanDam04@users.noreply.github.com>
2022-12-12 11:24:17 +0100

    Update Pflicht-Offline-Aufgabe 05-06.cpp

--- Jenkins/Pflicht-Offline-Aufgabe 05-06.cpp
+++ Jenkins/Pflicht-Offline-Aufgabe 05-06.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <string>
+#include <climits>
 
 using namespace std;
 
 int main()
 {
-    int ar[5], ar_c[6];
-    int i = 0, max = -10000, min = 100000;
-    for (int i = 0; i < 5; i++)
+    int n = 5;
+    int ar[5];
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (int i = 1; i < n + 1; i++)
     {
-        cout << "Bitte geben Sie die " << i + 1 << ". Zahl ein: ? ";
+        cout << "Bitte geben Sie die " << i << ". Zahl ein: ? ";
         cin >> ar[i];
-        ar_c[i] = 0;
-        if (ar[i] > 6 || ar[i] < 1)
-            i--;
-        else{
-            if(ar[i] > max) max = ar[i];
-            if(ar[i] < min) min = ar[i];
-        }
-        
+        if (ar[i] > max)
+            max = ar[i];
+        if (ar[i] < min)
+            min = ar[i];
     }
-    for (int i = 0; i < 5; i++)
+    for (int i = 1; i < n + 1; i++)
     {
-        cout << "Die " << i + 1 << ". eingegebene Zahl lautete: " << ar[i] << endl;
+        cout << "Die " << i << ". eingegebene Zahl lautete: " << ar[i] << endl;
     }
+
     cout << "Die kleinste eingegebene Zahl lautete: " << min << endl;
     cout << "Die groesste eingegebene Zahl lautete: " << max << endl;
-
     system("pause");
     return 0;
 }
