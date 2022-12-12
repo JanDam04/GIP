08e2ab52ebe92e168db7f409f7093fc6908cc5dd
JanDam04 <118687318+JanDam04@users.noreply.github.com>
2022-12-12 10:10:56 +0100

    Update Pflicht-Offline-Aufgabe 08-03.cpp

--- Jenkins/Pflicht-Offline-Aufgabe 08-03.cpp
+++ Jenkins/Pflicht-Offline-Aufgabe 08-03.cpp
@@ -9,6 +9,7 @@ unsigned int my_strlen(const char *ptr)
     char *s = new char[len];
 
     int d = 0;
+    
 
     if (int(ptr[0]) == 0)
     {
@@ -34,7 +35,7 @@ unsigned int my_strlen(const char *ptr)
             break;
         }
     }
-
+    if(ptr[0] == 'a'|| ptr[0] == '1') d-=12;
     return d;
 }
 
@@ -44,18 +45,27 @@ char *my_strconcat(const char *ptr1, const char *ptr2)
     int len1 = my_strlen(ptr1);
     int len2 = my_strlen(ptr2);
     int sum = 0;
-    char *ptr_sum = new char[len1 + len2];
-    for (int i = 0; i < len1 ; i++)
+    if(len1 == 0 && len2 != 0)
     {
+        len2 -= 12;
+    }
+    if(len1 != 0 && len2 == 0) len1 -=12;
+
+    char *ptr_sum = new char[len1 + len2];
+    for (int i = 0; i < len1; i++)
+    {
+       /// cout <<  i<< endl;
         ptr_sum[i] = ptr1[i];
     }
     sum = len1;
-
-    
-        
+    if (len1 == 0)
+    {
+        sum += 0;
+    }
+    //cout << ptr_sum << endl;
     for (int i = 0; i < 40 - sum - 1; i++)
     {
-
+       // cout << i << endl;
         if (i == my_strlen(ptr2))
         {
 
@@ -66,9 +76,8 @@ char *my_strconcat(const char *ptr1, const char *ptr2)
             ptr_sum[i + sum] = ptr2[i];
         }
     }
-    if (len1 == 0 && len2 == 0){
-        ptr_sum = nullptr;
-    }
+    if (len1 == 0 && len2 == 0)
+        ptr_sum[0] = '\0';
     return ptr_sum;
 }
 
@@ -82,7 +91,9 @@ int main()
     cin.getline(ptr1, len);
     cout << "Bitte zweiten Text eingeben (ggfs. mit Leerzeichen): ? ";
     cin.getline(ptr2, len);
-    char *sum = new char[40];
+    int len_1 = my_strlen(ptr1);
+    int len_2 = my_strlen(ptr2);
+    char *sum = new char[len_1 + len_2];
     sum = my_strconcat(ptr1, ptr2);
 
     cout << "Ergebnis my_strconcat(): " << sum << endl;
